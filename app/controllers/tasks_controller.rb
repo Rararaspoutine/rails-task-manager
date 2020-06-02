ff28d1c52510bdd81c325a7b4ca3747d8989c79a
Damien Gagnepain <dg.raspoutine@gmail.com>
2020-06-02 16:48:13 +0200

    ex

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,31 @@
 class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    redirect_to task_path(Task.create(task_params))
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
 end
